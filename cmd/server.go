073ee9e3afb6eca195df1656f0afc1c3da4c985a
skay <skai-zhang@hotmail.com>
2023-07-13 16:41:37 +0800

    add api router

--- cmd/server.go
+++ cmd/server.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"log"
 	"p2w/global"
+	"p2w/service"
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
@@ -37,9 +38,13 @@ var serverCmd = &cobra.Command{
 		} else if serConf.port != global.GetInt("server.port") {
 			global.Set("server.port", serConf.port)
 		}
-		// 启动服务
+		// 加载路由
 		log.Println("HTTP server startup in progress ->", serConf.port)
 		r := gin.Default()
+		http := service.HTTP{}
+		r.GET("/api/pdf", http.ApiToPDF)
+		r.GET("/api/img", http.ApiToImage)
+		// 启动服务
 		r.Run(fmt.Sprint(":", serConf.port))
 		log.Println("HTTP server has stopped")
 	},
---
+++ service/http.go
@@ -0,0 +1,13 @@
+package service
+
+import "github.com/gin-gonic/gin"
+
+type HTTP struct{}
+
+func (http HTTP) ApiToPDF(ctx *gin.Context) {
+
+}
+
+func (http HTTP) ApiToImage(ctx *gin.Context) {
+
+}
