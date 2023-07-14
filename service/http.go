f0671ef872586d67b32a72505dedeb14cf9f6d8c
skay <skai-zhang@hotmail.com>
2023-07-14 16:06:39 +0800

    add api server

--- service/http.go
+++ service/http.go
@@ -7,14 +7,60 @@ Copyright © 2023 SkyeZhang <skai-zhang@hotmail.com>
 
 package service
 
-import "github.com/gin-gonic/gin"
+import (
+	"p2w/global"
+
+	"github.com/gin-gonic/gin"
+)
 
 type HTTP struct{}
 
 func (http HTTP) ApiToPDF(ctx *gin.Context) {
-
+	// 地址
+	url := ctx.Query("url")
+	if len(url) == 0 {
+		global.ReturnMessage(ctx, false, "url cannot be empty")
+		return
+	}
+	// 访问地址截取页面内容
+	cache := ToPDF(url)
+	// 发送接口(填了就不放回PDF而是直接发送)
+	send := ctx.Query("send")
+	if len(url) == 0 {
+		global.ReturnFile(ctx, "application/pdf", cache)
+	} else {
+		// 标识码
+		code := global.GetCode(ctx.Query("code"))
+		global.SendFile(send, cache, code+".pdf")
+		global.ReturnSuccess(ctx, true)
+	}
 }
 
 func (http HTTP) ApiToImage(ctx *gin.Context) {
-
+	// 地址
+	url := ctx.Query("url")
+	if len(url) == 0 {
+		global.ReturnMessage(ctx, false, "url cannot be empty")
+		return
+	}
+	// 截取元素
+	element := ctx.Query("element")
+	// 图片质量
+	quality := ctx.Query("quality")
+	var cache []byte
+	if len(url) == 0 {
+		cache = ToFullImage(url, global.ToInt(quality, 90))
+	} else {
+		cache = ToElementImage(url, element)
+	}
+	// 发送接口(填了就不放回PDF而是直接发送)
+	send := ctx.Query("send")
+	if len(url) == 0 {
+		global.ReturnFile(ctx, "image/png", cache)
+	} else {
+		// 标识码
+		code := global.GetCode(ctx.Query("code"))
+		global.SendFile(send, cache, code+".png")
+		global.ReturnSuccess(ctx, true)
+	}
 }
